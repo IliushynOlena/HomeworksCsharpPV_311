64cd37fa5c33a1d12dda54800fd0bb1bb5fd2a61
ilusin_o@itstep.academy <ilusin_o@itstep.academy>
2024-06-14 18:22:25 +0300

    Add project files.

---
+++ 01_HomeworkIntroToCsharp/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace _01_HomeworkIntroToCsharp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
